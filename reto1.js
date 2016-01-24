b742e13dc6661892e321f1e67945cea092b68a90
Edwin Ortiz <edwinortizpro@gmail.com>
2016-01-23 22:31:25 -0500

    Creando mi servidor

---
+++ reto1.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var faker = require('faker');
+var _ = require('lodash');
+
+var generarUsuario = function(){
+  var randomId=faker.random.uuid();
+  var randomName = faker.name.findName();
+  var randomContent=faker.lorem.words();
+  var randomDate = faker.date.past();
+  var randomImage = faker.image.people();
+  return {
+    id: randomId,
+    nombre: randomName,
+    contenido: randomContent,
+    fecha: randomDate,
+    imagen: randomImage,
+  }
+
+}
+
+app.get('/', function (req, res) {
+  res.send('Mi primer servidor!');
+})
+
+app.get('/friends', function (req, res) {
+  var cantidad = _.random(5,10)
+  var usuarios = _.times(cantidad, generarUsuario)
+  res.json(usuarios);
+})
+
+app.get('/amigos', function (req, res) {
+  res.send('Mis amigos');
+})
+
+
+app.listen(3000);
